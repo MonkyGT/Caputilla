a1f15d3a96bcfbd3c362fbfe3d6dd2698ec843a6
Lofiat <dyeddaboi@gmail.com>
2025-06-25 17:31:16 -0500

    Fixed OnJoin issues

---
+++ Caputilla/Patches/FusionHubPatches.cs
@@ -0,0 +1,26 @@
+﻿using Caputilla.Utils;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppFusion;
+
+namespace CapuchinTemplate.Patches
+{
+    [HarmonyPatch(typeof(FusionHub), "OnPlayerJoined")]
+    public class FusionHubPatchesJoin
+    {
+        public static void Postfix(FusionHub __instance, PlayerRef player)
+        {
+            if (FusionHub.Runner.LocalPlayer == player)
+                RoomUtils.instance.OnJoin();
+        }
+    }
+    
+    [HarmonyPatch(typeof(FusionHub), "Leave")]
+    public class FusionHubPatchesLeave
+    {
+        public static void Postfix(FusionHub __instance)
+        {
+            RoomUtils.instance.OnLeave();
+        }
+    }
+}
